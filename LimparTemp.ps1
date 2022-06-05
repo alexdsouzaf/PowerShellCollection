1cd06f6fba4653b732478c48424addffc75256ad
alexdsouzaf <81589945+alexdsouzaf@users.noreply.github.com>
2022-06-05 15:30:33 -0300

    Add files via upload

--- LimparTemp.ps1
+++ LimparTemp.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "SilentlyContinue" 
 Set-Location C:\Users\User\AppData\Local\Temp
+echo "removendo arquivos..."
 foreach($arquivo in Get-ChildItem)
 {
     Remove-Item $arquivo -Recurse -Force
